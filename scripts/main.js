e5549e76c1c50b133bca1ee66c6816607343a23d
Meredith Lampe <merlam@fb.com>
2018-07-25 23:12:52 -0700

    style parties unit

--- scripts/main.js
+++ scripts/main.js
@@ -241,19 +241,8 @@ function createPartyElement(partyId, host, date, location, hostPic) {
               '<div class="username mdl-color-text--black"></div>' +
             '</div>' +
           '</div>' +
-          '<span class="star">' +
-            '<div class="not-starred material-icons">star_border</div>' +
-            '<div class="starred material-icons">star</div>' +
-            '<div class="star-count">0</div>' +
-          '</span>' +
-          '<div class="text"></div>' +
-          '<div class="comments-container"></div>' +
-          '<form class="add-comment" action="#">' +
-            '<div class="mdl-textfield mdl-js-textfield">' +
-              '<input class="mdl-textfield__input new-comment" type="text">' +
-              '<label class="mdl-textfield__label">Comment...</label>' +
-            '</div>' +
-          '</form>' +
+          '<div class="date"></div>' +
+          '<div class="location"></div>' + 
         '</div>' +
       '</div>';
 
@@ -261,12 +250,10 @@ function createPartyElement(partyId, host, date, location, hostPic) {
   var div = document.createElement('div');
   div.innerHTML = html;
   var postElement = div.firstChild;
-  if (componentHandler) {
-    componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
-  }
 
   // Set values.
-  postElement.getElementsByClassName('text')[0].innerText = date;
+  postElement.getElementsByClassName('date')[0].innerText = date;
+  postElement.getElementsByClassName('location')[0].innerText = location;
   postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = host + '\'s Party';
   // postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
   postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
